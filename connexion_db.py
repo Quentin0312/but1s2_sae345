4c138085a9ba038092b4f83aa7fdd4087dbfe1b3
Quentin Payet <quentinpayet1203@gmail.com>
2025-01-23 14:54:08 +0100

    pythonanywhere dabatase credentials specified

--- connexion_db.py
+++ connexion_db.py
@@ -7,11 +7,11 @@ def get_db():
     if db is None:
         #
         db = g._database = pymysql.connect(
-            host="localhost",
+            host="quentinpayet.mysql.pythonanywhere-services.com",
             # host="serveurmysql",
             user="quentinpayet",
-            password="1963",
-            database="but1s2sae345",
+            password="azerty97480",
+            database="quentinpayet$sae345",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
